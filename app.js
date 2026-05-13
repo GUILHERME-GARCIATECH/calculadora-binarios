e67359fb541506039f1ace2c1af56e6ec9baf05d
Guilherme Garcia <guilherme.garciatech@gmail.com>
2026-05-13 15:08:39 -0400

    a

--- app.js
+++ app.js
@@ -1,29 +1,40 @@
 const inverter = (str) => str.split('').reverse().map(Number);
-const newArray = (str) => str.split('').map(Number);
 
 let res = document.getElementById('res');
 
 function calcular(){
     const opr = document.getElementById('operacao').value;
     const num = document.getElementById('input').value;
-
     
+    let resultadoFinal = "";
+    let tipoResultado = "";
+
     if(opr === "decimal-binario"){
-         const resultadoFinal = converterParaBinario(num);
+        resultadoFinal = converterParaBinario(num);
+        tipoResultado = "binário";
     }
     if(opr === "binario-decimal"){
-        const resultadoFinal = converterParaDecimal(num);
+        resultadoFinal = converterParaDecimal(num);
+        tipoResultado = "decimal";
     }
    
-
-    res.innerHTML = `<p>O seu número em decimal é ${resultadoFinal}</p>`
+    res.innerHTML = `<p>O seu número em <strong>${tipoResultado}</strong> é: <strong>${resultadoFinal}</strong></p>`;
 }
-// [2, 5, 6] => [6, 5, 2]
-function converterParaBinario(num){
-    let binario = [];
-    for (let i = 0; i < num.length; i++){
-        binario
-    }
+
+function converterParaBinario(numStr){
+
+    let num = parseInt(numStr, 10);
+
+    if (num === 0) return 0;
+
+    let restos = [];
+
+   while (num > 0){
+    restos.push(num % 2);
+    num = Math.floor(num / 2);
+   }
+
+   return restos.reverse().join('');
 }
 
 
